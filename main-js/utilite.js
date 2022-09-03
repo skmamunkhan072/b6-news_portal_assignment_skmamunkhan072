797f1fcb017c732f0341f05e9c478dce43f950b4
Sk Mamun Khan <skmamunkhan072@gmail.com>
2022-09-03 13:05:17 +0600

    dainamick div creat

--- main-js/utilite.js
+++ main-js/utilite.js
@@ -1,3 +1,4 @@
+// all data load api function
 const allDataLodeApi = async (allData) => {
   const url = `https://openapi.programming-hero.com/api/news/category/01`;
   const res = await fetch(url);
@@ -5,9 +6,17 @@ const allDataLodeApi = async (allData) => {
   const data = dataLod.data;
   return data;
 };
-
+// single data function
 const allDataLode = async () => {
   const allData = await allDataLodeApi();
-  console.log(allData);
+  allData.forEach((data) => {
+    console.log(data);
+  });
+};
+// creat all card dainamic and disply  my website
+const creadDainameckCard = (data) => {
+  const cardContainer = document.getElementById("all-card-container");
+  const cardWraper = document.createElement("div");
+  cardWraper.classList.add("col-12");
 };
 allDataLode();
